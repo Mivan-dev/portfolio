5921166b8ee8ec9a2b2768640e103583e00d11b8
Ivan-M <sirius.ivo@gmail.com>
2026-07-01 19:39:11 -0300

    feat: restructure project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Nav } from './layout/nav/nav';
 import { Footer } from './layout/footer/footer';
-import { Proyects } from './features/proyects/proyects';
-import { Skills } from './features/skills/skills';
-import { About } from './features/about/about';
-import { Hero } from './features/hero/hero';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Footer, Hero, About, Skills, Proyects],
+  imports: [Nav, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
