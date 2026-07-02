0b9c04a07ea697a099466645b2a49d43d623afea
Ivan-M <sirius.ivo@gmail.com>
2026-07-02 16:47:03 -0300

    feat: enhance ProjectCard component with input properties and image navigation methods

--- src/app/features/sections/projects/project-card/project-card.ts
+++ src/app/features/sections/projects/project-card/project-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './project-card.css',
 })
 export class ProjectCard {
+
+  currentImage = signal(0);
+  images = input<string[]>([])
+  title = input<string>('')
+  description = input<string>('')
+  technologies = input<string[]>([])
+
+  nextImage(){
+    this.currentImage.update(i => (i + 1) % this.images().length)
+  }
+
+  prevImage(){
+    this.currentImage.update(i => (i - 1 + this.images().length) % this.images().length)
+  }
+
+  goToImage(index: number){
+    this.currentImage.set(index)
+  }
 
 }
