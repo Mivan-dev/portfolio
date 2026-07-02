8c8cd9839668453ddb9465b47b353af125e54dcd
Ivan-M <sirius.ivo@gmail.com>
2026-07-02 02:30:23 -0300

    feat: update Home component to include sections for Hero, About, Skills, Projects, and Contact

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Contact } from "../sections/contact/contact";
+import { Hero } from "../sections/hero/hero";
+import { About } from "../sections/about/about";
+import { Skills } from "../sections/skills/skills";
+import { Projects } from '../sections/projects/projects';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Contact, Hero, About, Skills, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
