c008b22d648507f13a6b0e0f8b3cbdffdc0eb157
Ivan-M <sirius.ivo@gmail.com>
2026-07-07 01:47:31 -0300

    feat: implement project display with ProjectCard component in projects section

--- src/app/features/sections/projects/projects.ts
+++ src/app/features/sections/projects/projects.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ProjectCard } from './project-card/project-card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+
+  projects = [
+  {
+    title: 'Kiosco App',
+    description: '...',
+    images: ['assets/images/kiosco-1.png', 'assets/images/kiosco-2.png', 'assets/images/kiosco-3.png'],
+    technologies: ['Angular', 'NestJS', 'PostgreSQL']
+  },
+  {
+    title: 'Expenses Manager',
+    description: '...',
+    images: ['assets/images/expenses-1.png', 'assets/images/expenses-2.png', 'assets/images/expenses-3.png'],
+    technologies: ['Angular', 'Tailwind']
+  }
+];
 
 }
