2f3ff5d8b9a08b9be782dc2345a423a59584f4d6
Ivan-M <sirius.ivo@gmail.com>
2026-07-09 01:38:43 -0300

    feat: implement contact form with reactive form validation

--- src/app/features/sections/contact/contact.ts
+++ src/app/features/sections/contact/contact.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+
+  fb = inject(FormBuilder)
+
+  form = this.fb.group({
+    nombre: ["", Validators.required],
+    email: ["", Validators.required],
+    telefono: ["", Validators.required],
+    mensaje: ["", Validators.required],
+  })
+
+  onSubmit(){}
 
 }
