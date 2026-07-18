6a4de5aa09bee8fcd186f835fb4cf954f49479db
Ivan-M <sirius.ivo@gmail.com>
2026-07-18 01:39:16 -0300

    fix: update project URLs and descriptions for accuracy and clarity

--- src/app/features/sections/projects/projects.ts
+++ src/app/features/sections/projects/projects.ts
@@ -20,22 +20,22 @@ export class Projects {
         'images/student-system-4.png',
       ],
       technologies: ['Angular', 'Bootstrap', 'Angular Material', 'MockApi'],
-      url: 'https://github.com/Mivan-dev/todo-app',
+      url: 'https://github.com/Mivan-dev/entrega1-ivan-mendoza',
     },
     {
       title: 'Expenses Manager',
       description:
-        'Aplicación personal para llevar gastos de tarjetas de crédito y servicios con guardado en LocalStorage. Proximamente con db propia en PostgreSQL y NestJS.',
+        'Aplicación para llevar gastos de tarjetas de crédito y servicios con guardado en LocalStorage. En desarrollo DB con NestJS y PostgreSQL.',
       images: ['images/expenses-1.png', 'images/expenses-2.png', 'images/expenses-3.png'],
       technologies: ['Angular', 'Tailwind', 'NestJS', 'SQL'],
       url: 'https://github.com/Mivan-dev/expenses-manager',
     },
     {
       title: 'Todo App',
-      description: 'Aplicación para la creación y seguimiento de tareas personales.',
+      description: 'Aplicación para la creación y seguimiento de tareas personales con guardado en LocalStorage.',
       images: ['images/todo-1.png', 'images/todo-2.png', 'images/todo-3.png'],
       technologies: ['Angular', 'Bootstrap', 'TypeScript'],
-      url: 'https://github.com/Mivan-dev/todo-app',
+      url: 'https://app-todo-mivan.netlify.app/',
     },
   ];
 }
