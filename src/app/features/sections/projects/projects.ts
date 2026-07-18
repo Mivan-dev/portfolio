880329d8189fdcad1838e1442ca925d57e84eea5
Ivan-M <sirius.ivo@gmail.com>
2026-07-18 01:21:20 -0300

    fix: reorder project entries for improved clarity and consistency

--- src/app/features/sections/projects/projects.ts
+++ src/app/features/sections/projects/projects.ts
@@ -8,29 +8,34 @@ import { ProjectCard } from './project-card/project-card';
   styleUrl: './projects.css',
 })
 export class Projects {
-
   projects = [
-  {
-    title: 'Expenses Manager',
-    description: 'Aplicación personal para llevar gastos de tarjetas de crédito y servicios con guardado en LocalStorage. Proximamente con db propia en PostgreSQL y NestJS.',
-    images: ['images/expenses-1.png', 'images/expenses-2.png', 'images/expenses-3.png'],
-    technologies: ['Angular', 'Tailwind', 'NestJS', 'SQL'],
-    url: 'https://github.com/Mivan-dev/expenses-manager'
-  },
-  {
-    title: 'Todo App',
-    description: 'Aplicación para la creación y seguimiento de tareas personales.',
-    images: ['images/todo-1.png', 'images/todo-2.png', 'images/todo-3.png'],
-    technologies: ['Angular', 'Bootstrap', 'TypeScript'],
-    url: 'https://github.com/Mivan-dev/todo-app'
-  },
-  {
-    title: 'Otro Proyecto',
-    description: 'Bla bla bla blablubla blalalalalablala liblabliblo blubliblato',
-    images: ['images/exp-1.jpg', 'images/exp-2.jpg', 'images/exp-3.jpg'],
-    technologies: ['Laravel', 'php'],
-    url: 'https://github.com/Mivan-dev/todo-app'
-  }
-];
-
+    {
+      title: 'Student Management System',
+      description:
+        'Sistema de gestión de estudiantes, que permite administrar estudiantes, cursos e inscripciones* con diferentes niveles de acceso según el rol del usuario. Info detallada en el README.md',
+      images: [
+        'images/student-system-2.png',
+        'images/student-system-1.png',
+        'images/student-system-3.png',
+        'images/student-system-4.png',
+      ],
+      technologies: ['Angular', 'Bootstrap', 'Angular Material', 'MockApi'],
+      url: 'https://github.com/Mivan-dev/todo-app',
+    },
+    {
+      title: 'Expenses Manager',
+      description:
+        'Aplicación personal para llevar gastos de tarjetas de crédito y servicios con guardado en LocalStorage. Proximamente con db propia en PostgreSQL y NestJS.',
+      images: ['images/expenses-1.png', 'images/expenses-2.png', 'images/expenses-3.png'],
+      technologies: ['Angular', 'Tailwind', 'NestJS', 'SQL'],
+      url: 'https://github.com/Mivan-dev/expenses-manager',
+    },
+    {
+      title: 'Todo App',
+      description: 'Aplicación para la creación y seguimiento de tareas personales.',
+      images: ['images/todo-1.png', 'images/todo-2.png', 'images/todo-3.png'],
+      technologies: ['Angular', 'Bootstrap', 'TypeScript'],
+      url: 'https://github.com/Mivan-dev/todo-app',
+    },
+  ];
 }
